f21ad8511ae9c2ef5632a53e69505dd6f3d14fa7
Grigoriy Romanyuk <skorovodkin@icloud.com>
2019-03-13 20:57:16 -0700

    Move flipped card search to the extension.\ + Test of email commit

--- cs193p-Concentration/Concentration.swift
+++ cs193p-Concentration/Concentration.swift
@@ -14,17 +14,7 @@ class Concentraion
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp } .oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -54,5 +44,11 @@ class Concentraion
             cards += [card, card]
         }
         cards.shuffle()
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
